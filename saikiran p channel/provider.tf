e3c217e8ce1d755588115dd8b05b649dde1b2e54
iam-narasimhasai <narasimhasai.nimmagadda@gmail.com>
2024-10-10 23:17:30 +0530

    backend

--- saikiran p channel/provider.tf
+++ saikiran p channel/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = "us-east-1"
 }
+terraform {
+  backend "s3" {
+    bucket         = "backendterraforms3"
+    key            = "global/s3/terraform.tfstate"
+    region         = "ap-south-1"
+    
+  }
+}
